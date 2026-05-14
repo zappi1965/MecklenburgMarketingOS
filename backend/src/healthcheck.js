6fc6c3787a1afaa98df26815ee6d689867082668
zappi1965 <88281702+zappi1965@users.noreply.github.com>
2026-05-14 10:37:46 +0200

    Delete backend directory

--- backend/src/healthcheck.js
+++
@@ -1,4 +0,0 @@
-
-const http = require('http')
-const url = process.env.HEALTHCHECK_URL || `http://localhost:${process.env.PORT || 4000}/api/production/health`
-http.get(url, res => process.exit(res.statusCode === 200 ? 0 : 1)).on('error', () => process.exit(1))
